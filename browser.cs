779839944d9c9640603896108d5cbecdc7eda29d
eniepa <apeine.k@gmail.com>
2020-07-28 14:11:20 +0300

    Add project files.

---
+++ browser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace ChromiumBrowser
+{
+    public partial class Browser : Form
+    {
+
+        private ChromiumWebBrowser browser;
+
+        public Browser()
+        {
+            InitializeComponent();
+            InitializeBrowser();
+        }
+
+        private void InitializeBrowser()
+        {
+            Cef.Initialize(new CefSettings());
+            browser = new ChromiumWebBrowser("https://datorium.eu");
+            browser.Dock = DockStyle.Fill;
+            this.Controls.Add(browser);
+        } 
+    }
+}
